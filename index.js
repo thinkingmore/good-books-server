6f1886cafa6b1c5a89b8ea5ff79920bdb0af68b8
thinkingmore <peterkevin823@gmail.comm>
2022-11-24 15:17:38 +0600

    established database connection and added api to get product categories

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
-
 const app = express();
 
 // middleware
@@ -9,8 +10,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async(req,res)=>{
+// establish database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ltn8juo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+
+const run = async()=>{
+    try{
+        const categoriesCollection = client.db("goodBooks").collection("categories");
+    
+        // API path for running CURD operation
+        app.get('/categories',async(req,res)=>{
+            const query= {}
+            const cursor = categoriesCollection.find(query)
+            const categories = await cursor.toArray();
+            res.send(categories);
+        })
+
+    }
+    
+    finally{
+    
+    }
+}
+
+run().catch(error=> console.error(error));
+
+app.get('/',(req, res) => {
     res.send('good books server is running')
 })
+
+
 
 app.listen(port,()=> console.log(`good books server running on ${port}`))
